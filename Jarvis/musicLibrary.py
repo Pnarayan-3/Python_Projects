dd06b359d80af25daae91c2450499a69ebd99d51
Pushkar N <131258869+Pnarayan-3@users.noreply.github.com>
2025-11-30 13:45:41 +0530

    Update musicLibrary.py

--- Jarvis/musicLibrary.py
+++ Jarvis/musicLibrary.py
@@ -1,6 +1,6 @@
 music={
-    "Deewane":"https://www.youtube.com/watch?v=B-2BCSxnyHA&list=RDB-2BCSxnyHA&start_radio=1",
-    "Afterhours":"https://www.youtube.com/watch?v=NsWy4kxQ9cE&list=RDB-2BCSxnyHA&index=2",
-    "Tu":"https://www.youtube.com/watch?v=4dkss90fdPc&list=RDB-2BCSxnyHA&index=3"
+    "Lost Sky":"https://www.youtube.com/watch?v=S19UcWdOA-I&list=RDyJg-Y5byMMw&index=2",
+    "Warriyo":"https://www.youtube.com/watch?v=yJg-Y5byMMw&list=RDyJg-Y5byMMw&start_radio=1",
+    "Capaque":"https://www.youtube.com/watch?v=u13mOCWZ81o&list=RDu13mOCWZ81o&start_radio=1"
 
 }
